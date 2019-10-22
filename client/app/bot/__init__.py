52da60e7caf65e3274c156567bf5dde201fc1a53
Illya Maltsev <imaltsev98@gmail.com>
2019-10-22 13:29:42 +0300

    try to add State patern to bot

--- client/app/bot/__init__.py
+++ client/app/bot/__init__.py
@@ -1,10 +1,10 @@
 import os
 from flask import Blueprint
 
-from .PCBot import PCBot
+from .StateBot import StateBot
 
 telegram = Blueprint('telegram', __name__)
-bot = PCBot(os.getenv('BOT_TOKEN'))
+bot = StateBot(os.getenv('BOT_TOKEN'))
 bot.set_webhook(os.getenv('NGROK_URL') + '/telegram/web_hook/')
 
 from . import routes, handlers
--- client/app/bot/handlers.py
+++ client/app/bot/handlers.py
@@ -1,4 +1,5 @@
 import logging
+from telebot.types import CallbackQuery, Message
 
 from . import bot
 from . import texts
@@ -7,19 +8,66 @@ logger = logging.getLogger(__name__)
 
 
 @bot.message_handler(commands=['start'])
-def start_handler(msg):
+def start_handler(msg: Message):
 
     """This handler runs when someone send '/start' to bot in Telegram"""
 
-    logger.info(msg)
+    logger.debug(msg)
 
     tg_id = msg.from_user.id
     f_name = msg.from_user.first_name or ''
     l_name = msg.from_user.last_name or ''
 
-    logger.info(f'START id:{tg_id} name:{f_name} {l_name}')
+    logger.debug(f'START HANDLER id:{tg_id} name:{f_name} {l_name}')
 
-    # TODO: send request to server
+    # TODO: send request to server (check may be this user is already exist)
 
-    bot.send_message(tg_id, texts.start_message)
-    bot.change_state(tg_id, 1)
+    msg_text = texts.start_message
+    bot.change_state(tg_id, state=1, header_text=msg_text)
+
+
+def button_handler(call: CallbackQuery, buttons: list) -> bool:
+    try:
+        button_name = call.data.split(' ')[0]
+        return button_name in buttons
+    except Exception as e:
+        logger.error(e)
+        return False
+
+
+@bot.callback_query_handler(lambda call: button_handler(call, buttons=['create_card']))
+def create_card(call: CallbackQuery):
+    """"""
+
+    logger.debug(call)
+
+    tg_id = call.from_user.id
+
+    # TODO: send request to server to create cart
+
+    response = True
+    card = "1234 1234 1234 1234"
+
+    msg_text = texts.new_card.format(card) if response else texts.card_limit
+    bot.change_state(tg_id, state=1, header_text=msg_text, call=call)
+
+
+@bot.callback_query_handler(lambda call: button_handler(call, buttons=['choose_card']))
+def create_card(call: CallbackQuery):
+    """"""
+
+    logger.debug(call)
+
+    tg_id = call.from_user.id
+
+    # TODO: send request to server to create cart
+
+    response = ["1234 1234 1234 1234", "1234 1234 1234 5555", "1234 1234 1234 6545"]
+    card = "1234 1234 1234 1234"
+
+    msg_text = texts.new_card.format(card) if response else texts.card_limit
+    bot.change_state(tg_id, state=1, header_text=msg_text, call=call)
+
+
+
+
